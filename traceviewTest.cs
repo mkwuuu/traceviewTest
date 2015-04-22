a062829ac84d1515388d4accbdc19edeba7994da
mkwuuu <mikehanwu@hotmail.com>
2015-04-22 07:39:30 -0700

    Syntax changes

--- traceviewTest.cs
+++ traceviewTest.cs
@@ -4,23 +4,23 @@ using System.Text;
 using System.IO;
 
 
-    public class traceviewTest
+    public class traceviewTest
     {
 
-        public static void Main ()
+        public static void Main ()
         {
-            public static var url = “www.appneta.com”;
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create (url);
+            const string url = "http://www.appneta.com";
+            var request = (HttpWebRequest)WebRequest.Create (url);
 
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse ();
+            var response = (HttpWebResponse)request.GetResponse ();
 
             Console.WriteLine ("Content length is {0}", response.ContentLength);
             Console.WriteLine ("Content type is {0}", response.ContentType);
 
-            Stream receiveStream = response.GetResponseStream ();
-            StreamReader readStream = new StreamReader (receiveStream, Encoding.UTF8);
+            var receiveStream = response.GetResponseStream ();
+            var readStream = new StreamReader (receiveStream, Encoding.UTF8);
 
-            Console.WriteLine (“Reading page…”);
+            Console.WriteLine ("Reading appneta...");
             Console.WriteLine (readStream.ReadToEnd ());
             response.Close ();
             readStream.Close ();
@@ -29,12 +29,10 @@ using System.IO;
 
 /*
 The output looks like:
-
 Content length is 8070
 Content type is text/plain; charset=utf-8
 Reading page…
 <!DOCTYPE html>
 ...
 </html>
-
 */
